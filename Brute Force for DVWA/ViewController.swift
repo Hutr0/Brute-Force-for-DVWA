c576e6770544f9968ac5f93d69f6a971cf693bbd
Hutr0 <Hutro-games@yandex.ru>
2021-11-14 22:22:22 +0300

    POST request

--- Brute Force for DVWA/ViewController.swift
+++ Brute Force for DVWA/ViewController.swift
@@ -31,6 +31,29 @@ class ViewController: NSViewController {
     }
     
     @IBAction func postTapped(_ sender: NSButton) {
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
+        let parameters = ["Username": "hutr0", "Message": "Fuck u!"]
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type") 
+        guard let httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: []) else { return }
+        request.httpBody = httpBody
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let response = response {
+                print(response)
+            }
+            
+            guard let data = data else { return }
+            print(data)
+            
+            do {
+                let json = try JSONSerialization.jsonObject(with: data, options: [])
+                print(json)
+            } catch {
+                print(error.localizedDescription)
+            }
+        }.resume()
     }
 }
 
