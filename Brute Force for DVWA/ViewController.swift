3a6530567060c83265a622c386743341467d6129
Hutr0 <Hutro-games@yandex.ru>
2021-11-14 22:12:39 +0300

    GET method

--- Brute Force for DVWA/ViewController.swift
+++ Brute Force for DVWA/ViewController.swift
@@ -9,18 +9,28 @@ import Cocoa
 
 class ViewController: NSViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    @IBAction func getTapped(_ sender: NSButton) {
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
+        
+        let session = URLSession.shared
+        session.dataTask(with: url) { data, response, error in
+            if let response = response {
+                print(response)
+            }
+            
+            guard let data = data else { return }
+            print(data)
+            
+            do {
+                let json = try JSONSerialization.jsonObject(with: data, options: [])
+                print(json)
+            } catch {
+                print(error.localizedDescription)
+            }
+        }.resume()
     }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
-        }
+    
+    @IBAction func postTapped(_ sender: NSButton) {
     }
-
-
 }
 
